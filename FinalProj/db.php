b64692c7b5e13ce1c611346fe552e2f1d38b95ed
zbonus <31708229+zbonus@users.noreply.github.com>
2021-12-06 11:25:59 -0600

    Update db.php

--- FinalProj/db.php
+++ FinalProj/db.php
@@ -2,7 +2,7 @@
 // Server connection info
 define('DB_SERVER', 'classdb.it.mtu.edu');
 define('DB_USERNAME', 'zcbonus');
-define('DB_PASSWORD', 'Linear.327');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'zcbonus');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
